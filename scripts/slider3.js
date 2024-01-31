7aab4543d79d3cad4cceeda4c2d64e21eff0ed25
Sergey Maximov <dev9092336062@gmail.com>
2024-01-31 15:12:38 +0300

    slider3: dots

--- scripts/slider3.js
+++ scripts/slider3.js
@@ -2,17 +2,17 @@ const slider = document.querySelector(`[data-slider3]`);
 const leftBtn = document.querySelector(`[data-slider3-left]`);
 const rightBtn = document.querySelector(`[data-slider3-right]`);
 const dotListContainer = document.querySelector(`[data-slide3-dotlist]`);
-const dotList = dotListContainer?.querySelectorAll("button");
+const dotList = dotListContainer?.querySelectorAll("li");
 const plane = document.querySelector(`[data-slider3-img]`);
 
 const PLANE_ACTIVE_CLASSNAME = "_animate";
+const ACTIVE_CLASSNAME = "_active";
+const WIDTH_SLIDE = 370;
 
 let isScroll = false;
 
-const onScroll = (event) => {
-  if (!event) {
-    return;
-  }
+const flyPlane = () => {
+  if (isScroll) return;
 
   isScroll = true;
 
@@ -32,12 +32,31 @@ const onScroll = (event) => {
   }, 2100);
 };
 
+const animateDots = () => {
+  if (!dotList || !dotList.length) return;
+
+  const scrollWidth = slider.scrollLeft;
+  const activeNum = Math.floor((scrollWidth + WIDTH_SLIDE / 2) / WIDTH_SLIDE);
+
+  dotList.forEach((dot) => dot.classList.remove(ACTIVE_CLASSNAME));
+  dotList[activeNum] && dotList[activeNum].classList.add(ACTIVE_CLASSNAME);
+};
+
+const onScroll = (event) => {
+  if (!event) {
+    return;
+  }
+
+  flyPlane();
+  animateDots();
+};
+
 const slider3Control = () => {
   if (!slider) {
     return;
   }
 
-  slider.addEventListener("scroll", (event) => !isScroll && onScroll(event));
+  slider.addEventListener("scroll", (event) => onScroll(event));
 };
 
 export default slider3Control;
